d43b6304bab98d03e2f7625dc3ef43decd7bb5b8
Bito Mann <bito@bitomann.com>
2020-01-18 14:03:28 -0600

    pet sounds

---
+++ main.js
@@ -0,0 +1,31 @@
+// Create an object that represents your pet:
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+// Now that you have an object that represents your pet, it's time to add some behavior. Pick three behaviors that your pet has and add three kys to your pet object. Each key's value should be a function that logs to the console or displays a window alert. Your choice.
+
+// For example, if your pet is a dog, you could add a bark property, and the function would window.alert("WOOF!").
+
+// Once you have define the properties, invoke each behavior.
+const ourPupper = {
+    name: "Piper",
+    species: "Mut",
+    nicknames: ["Piper Diaper", "Pup Pup"],
+    age: 3,
+    bark: function (speakToo) {
+        window.alert(`${ourPupper.name} says "WOOF! WOOF!" to ${speakToo}`)
+    } ,
+    cry: function (sad) {
+console.log(`${ourPupper.name} ${sad} if we don't take him for a walk`)
+    },
+    pant: function (tired) {
+window.alert(`${ourPupper.nicknames} gets ${tired} after running!`)
+    }
+}
+ ourPupper.bark("Phoenix, Madeleine, Lili and Mamma")
+ ourPupper.cry("is soooo sad")
+ ourPupper.pant("so so tired")
+
+
